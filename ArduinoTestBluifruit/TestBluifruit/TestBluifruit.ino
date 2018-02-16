0159de60508d69ab9902d113fed998bc487cb924
Ciprian1995 <36262380+Ciprian1995@users.noreply.github.com>
2018-02-16 12:11:31 +0100

    a

--- ArduinoTestBluifruit/TestBluifruit/TestBluifruit.ino
+++ ArduinoTestBluifruit/TestBluifruit/TestBluifruit.ino
@@ -155,8 +155,21 @@ void loop(void)
       Serial.print("Received '1'");
       digitalWrite(LED, HIGH);
     }
+    else if((char)c == '3')
+  {
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);
+    } // waits 15ms for the servo to reach the position
+
   }
- // sweep();
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);
+  }// waits 15ms for the servo to reach the position
+  }
+ 
   delay(1000);
 }
 
@@ -172,13 +185,13 @@ void sweep()
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
       delay(15);
     } // waits 15ms for the servo to reach the position
-*/
+
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);
   }// waits 15ms for the servo to reach the position
-
+*/
 }
 
 
