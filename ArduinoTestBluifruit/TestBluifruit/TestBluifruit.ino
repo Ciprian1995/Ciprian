c16650f1bac0cf349f7473c09cbfefdfe7bc526a
KayUsvaltas <k.usvaltas@gmail.com>
2018-02-16 11:22:58 +0100

    m

--- ArduinoTestBluifruit/TestBluifruit/TestBluifruit.ino
+++ ArduinoTestBluifruit/TestBluifruit/TestBluifruit.ino
@@ -165,16 +165,17 @@ void loop(void)
 
 void sweep()
 {
-  int test =0;
-  if(test == 1)
-  
+  int test = ble.read();
+  Serial.print((char)c);
+  if((char)test == '3')
+  {
 for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);  
 } // waits 15ms for the servo to reach the position
   
-  
+  }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15); 
