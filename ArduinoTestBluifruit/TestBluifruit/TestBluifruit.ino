67b66bb9bac367f65217ec8472849c67cff2cb58
KayUsvaltas <k.usvaltas@gmail.com>
2018-02-16 11:25:40 +0100

    k

--- ArduinoTestBluifruit/TestBluifruit/TestBluifruit.ino
+++ ArduinoTestBluifruit/TestBluifruit/TestBluifruit.ino
@@ -180,7 +180,7 @@ for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15); 
   }// waits 15ms for the servo to reach the position
-  
+  // gg
 
 }
 
