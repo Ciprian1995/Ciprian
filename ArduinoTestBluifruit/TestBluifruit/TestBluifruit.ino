4e095ec7f9622fffcc8c9724c910aa52b0eef1c9
KayUsvaltas <k.usvaltas@gmail.com>
2018-02-16 11:17:45 +0100

    k

--- ArduinoTestBluifruit/TestBluifruit/TestBluifruit.ino
+++ ArduinoTestBluifruit/TestBluifruit/TestBluifruit.ino
@@ -16,8 +16,7 @@
 #endif
 
 #define LED 2
-<<<<<<< HEAD
-/* gg */
+
 =======
 #include <Servo.h> 
 int pos = 0;
@@ -168,21 +167,19 @@ void sweep()
 {
   int test =0;
   if(test == 1)
-  {
+  
 for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);  
 } // waits 15ms for the servo to reach the position
-  }
-  else if (test == 0)
-  {
+  
+  
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15); 
   }// waits 15ms for the servo to reach the position
-  }
-
+  
 
 }
 
