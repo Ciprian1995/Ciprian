3c74f8b33eb02b74c87781e149c7b3fcdb537c09
Ciprian1995 <36262380+Ciprian1995@users.noreply.github.com>
2018-02-21 14:46:34 +0100

    k

--- js/index.js
+++ js/index.js
@@ -103,10 +103,10 @@ function onSend(){
 }
 
 function disconnect(){
-	if (device.id == "F1:A4:8D:46:02:10") {
+	
 	ble.disconnect(deviceId, onDisconnect, onError);
 	document.getElementById("statusDiv").innerHTML = "Status: Disconnected";
-	}
+	
 }
 
 function onDisconnect(){
